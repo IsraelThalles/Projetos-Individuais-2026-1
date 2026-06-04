a46d53a66429fb86c2e662dd114578e314a461dc
Israel Thalles <santosisrael14@gmail.com>
2026-06-04 14:23:26 -0300

    Adiciona constantes para configuração do banco de dados e DDL

--- israel-thalles/projeto-4/codigos/constantes.py
+++ israel-thalles/projeto-4/codigos/constantes.py
@@ -2,6 +2,8 @@ from pathlib import Path
 
 PASTA_DADOS = Path("dados")
 PASTA_DADOS.mkdir(exist_ok=True)
+BANCO_DE_DADOS = "banco/banco.db"
+DDL = "banco/ddl.sql"
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0"
