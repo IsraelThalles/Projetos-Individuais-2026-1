964da6e09962adc0c978b224c65789c084ed1119
Israel Thalles <santosisrael14@gmail.com>
2026-06-04 16:23:02 -0300

    Atualiza constantes de caminho para utilizar o diretório base

--- israel-thalles/projeto-4/codigos/constantes.py
+++ israel-thalles/projeto-4/codigos/constantes.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
-PASTA_DADOS = Path("dados")
-PASTA_DADOS.mkdir(exist_ok=True)
-BANCO_DE_DADOS = "banco/banco.db"
-DDL = "banco/ddl.sql"
+DIRETORIO_BASE = Path(__file__).resolve().parent.parent
+BANCO_DE_DADOS = DIRETORIO_BASE / "banco" / "banco.db"
+DDL = DIRETORIO_BASE / "banco" / "ddl.sql"
+PASTA_DADOS = DIRETORIO_BASE / "dados"
+ARQUIVO_FONTES = DIRETORIO_BASE / "fonte_de_dados.json"
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0"
