50fd15d7629cd6aa8e457c9c49b147e8f848db53
Israel Thalles <santosisrael14@gmail.com>
2026-06-04 14:20:23 -0300

    Altera o tipo da coluna trimestre de TEXT para INTEGER na tabela catalogo_documentos

--- israel-thalles/projeto-4/banco/ddl.sql
+++ israel-thalles/projeto-4/banco/ddl.sql
@@ -2,7 +2,7 @@ CREATE TABLE catalogo_documentos (
     hash TEXT PRIMARY KEY,
     empresa TEXT NOT NULL,
     ano INTEGER,
-    trimestre TEXT,
+    trimestre INTEGER,
     nome_arquivo TEXT NOT NULL,
     caminho_local TEXT NOT NULL,
     url_origem TEXT NOT NULL,
