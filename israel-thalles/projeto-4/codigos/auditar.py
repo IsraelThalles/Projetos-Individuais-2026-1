c720f4bcb5e9a5b94cdbff38e651a556175bbfd6
Israel Thalles <santosisrael14@gmail.com>
2026-06-08 18:40:08 -0300

    Adiciona a verificação do estado do processamento

--- israel-thalles/projeto-4/codigos/auditar.py
+++ israel-thalles/projeto-4/codigos/auditar.py
@@ -9,12 +9,17 @@ from utilidades.hash import calcular_hash
 
 
 def auditar_documento(caminho_arquivo: Path) -> bool:
-    """Audita um documento verificando se ele já existe no banco de dados pelo seu hash. Se for um documento novo, salva suas informações no banco de dados."""
+    """Audita um documento verificando se ele já existe no banco de dados pelo seu hash e foi processado. Se for um documento novo, salva suas informações no banco de dados."""
     hash_do_documento = calcular_hash(caminho_arquivo)
     documento_existente = buscar_documento_por_hash(hash_do_documento)
 
     if documento_existente:
-        return False
+        if documento_existente.status_processamento == "CONCLUÍDO":
+            return False
+        
+        # Se parou no meio (PENDENTE ou ERRO), tenta de novo sem inserir de novo!
+        print(f"🔄 Retomando arquivo travado anteriormente: {caminho_arquivo.name}")
+        return True
     
     try:
         documento = Documento(
@@ -25,7 +30,7 @@ def auditar_documento(caminho_arquivo: Path) -> bool:
             nome_arquivo=caminho_arquivo.name,
             caminho_local=str(caminho_arquivo),
             data_ingestao=datetime.now().isoformat(),
-            status_processamento="Pendente"
+            status_processamento="PENDENTE"
         )
 
         print(f"Hash do documento: {documento.hash}")
