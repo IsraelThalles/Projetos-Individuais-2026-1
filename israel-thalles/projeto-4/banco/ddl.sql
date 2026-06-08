662876b56d300081bfd86b1ca219f247f99313f7
Israel Thalles <santosisrael14@gmail.com>
2026-06-08 00:22:02 -0300

    Muda nome da coluna 'empresa' para 'publicador' na tabela catalogo_documentos e adiciona a tabela metricas_operacionais

--- israel-thalles/projeto-4/banco/ddl.sql
+++ israel-thalles/projeto-4/banco/ddl.sql
@@ -1,6 +1,6 @@
 CREATE TABLE catalogo_documentos (
     hash TEXT PRIMARY KEY,
-    empresa TEXT NOT NULL,
+    publicador TEXT NOT NULL,
     ano INTEGER,
     trimestre INTEGER,
     nome_arquivo TEXT NOT NULL,
@@ -8,4 +8,15 @@ CREATE TABLE catalogo_documentos (
     url_origem TEXT NOT NULL,
     data_ingestao DATETIME DEFAULT CURRENT_TIMESTAMP,
     status_processamento TEXT DEFAULT 'PENDENTE'
+);
+
+CREATE TABLE metricas_operacionais (
+    identificador INTEGER PRIMARY KEY AUTOINCREMENT,
+    hash_documento TEXT NOT NULL,
+    empresa_referencia TEXT NOT NULL,
+    categoria TEXT NOT NULL,
+    nome_metrica TEXT NOT NULL,
+    valor REAL,
+    unidade_medida TEXT,
+    FOREIGN KEY (hash_documento) REFERENCES catalogo_documentos(hash) ON DELETE CASCADE
 );
