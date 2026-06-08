26d31392d96dc96e4c1de8458c4706a7d9ef02f7
Israel Thalles <santosisrael14@gmail.com>
2026-06-08 14:16:44 -0300

    Adiciona a constante para o arquivo de especificação

--- israel-thalles/projeto-4/codigos/utilidades/constantes.py
+++ israel-thalles/projeto-4/codigos/utilidades/constantes.py
@@ -5,6 +5,7 @@ BANCO_DE_DADOS = DIRETORIO_BASE / "banco" / "banco.db"
 DDL = DIRETORIO_BASE / "banco" / "ddl.sql"
 PASTA_DADOS = DIRETORIO_BASE / "dados"
 ARQUIVO_FONTES = DIRETORIO_BASE / "fonte_de_dados.json"
+ARQUIVO_PROMPT_SISTEMA = DIRETORIO_BASE / "prompts" / "prompt_sistema.md"
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0"
