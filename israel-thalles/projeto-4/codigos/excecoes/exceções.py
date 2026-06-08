c58c79492de8d16ee144ec4ae69f072502b86385
Israel Thalles <santosisrael14@gmail.com>
2026-06-08 10:43:52 -0300

    Adiciona a classe ErroDeExtracaoDeDados para tratar erros de extração de dados do nome do arquivo

--- israel-thalles/projeto-4/codigos/excecoes/exceções.py
+++ israel-thalles/projeto-4/codigos/excecoes/exceções.py
@@ -4,3 +4,12 @@ class ErroDeIdNaoEncontrado(ValueError):
         super().__init__(f"fmId não encontrado para {empresa} em {url}")
         self.empresa = empresa
         self.url = url
+
+
+
+class ErroDeExtracaoDeDados(ValueError):
+    """Erro ao extrair dados do nome do arquivo."""
+    def __init__(self, nome_arquivo: str, tipo_dado: str):
+        super().__init__(f"Não foi possível extrair {tipo_dado} de: {nome_arquivo}")
+        self.nome_arquivo = nome_arquivo
+        self.tipo_dado = tipo_dado
